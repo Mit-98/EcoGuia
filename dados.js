7e1c447a7b7165b2dc0c187f24b003e4c3714462
Milena Castro <71859646+Mit-98@users.noreply.github.com>
2024-09-07 00:09:26 -0300

    Update dados.js
    documentação do código adicionada

--- dados.js
+++ dados.js
@@ -1,23 +1,28 @@
+// Array de objetos que armazenam informações sobre a coleta seletiva em diferentes cidades.
 let dados = [
     {
+        // Informações sobre a cidade de Vargem Grande Paulista
         titulo : "Vargem Grande Paulista",
         descricao : "Coleta Seletiva: Terça, Quinta e Sabádo antes da coleta de lixo comum",
         link : "https://www.vargemgrandepaulista.sp.gov.br/site/coleta-seletiva/",
     },
 
     {
+        // Informações sobre a cidade de Cotia
         titulo : "Cotia",
         descricao : "coleta Seletiva: Todos os dias da semana",
         link : "https://cotia.sp.gov.br/dia-internacional-da-reciclagem-coleta-seletiva-passa-por-12-bairros-e-80-condominios-de-cotia/",
     },
 
     {
+        // Informações sobre a cidade de Itapevi
         titulo : "Itapevi",
         descricao : "Coleta Seletiva: Todos os dias da semana nos períodos da manhã, tarde e noite",
         link : "https://itapevi.sp.gov.br/coleta-de-lixo/",
     },
 
     {
+        // Informações sobre a cidade de Osasco
         titulo : "Osasco",
         descricao : "Coleta Seletiva: Todos os dias da semana nos períodos da manhã, tarde e noite",
         link : "https://osasco.sp.gov.br/digres/#1683116870581-06458f53-a9c8",
