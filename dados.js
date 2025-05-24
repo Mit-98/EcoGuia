0479f1bc4179886a699f87aea07b1b8260341fdc
Milena Castro <71859646+Mit-98@users.noreply.github.com>
2025-05-24 01:16:48 -0300

    Update dados.js
    adicionei mais dados e dei foco a somente uma cidade

--- dados.js
+++ dados.js
@@ -1,32 +1,80 @@
 // Array de objetos que armazenam informações sobre a coleta seletiva em diferentes cidades.
 let dados = [
-    {
-        // Informações sobre a cidade de Vargem Grande Paulista
-        titulo : "Vargem Grande Paulista",
-        descricao : "Coleta Seletiva: Terça, Quinta e Sabádo antes da coleta de lixo comum",
-        link : "https://www.vargemgrandepaulista.sp.gov.br/site/coleta-seletiva/",
-    },
+ // dados.js
 
-    {
-        // Informações sobre a cidade de Cotia
-        titulo : "Cotia",
-        descricao : "coleta Seletiva: Todos os dias da semana",
-        link : "https://cotia.sp.gov.br/dia-internacional-da-reciclagem-coleta-seletiva-passa-por-12-bairros-e-80-condominios-de-cotia/",
-    },
+  {
+    // Informações sobre a cidade de Vargem Grande Paulista (Objeto principal da cidade)
+    titulo: "Vargem Grande Paulista",
+    descricao: "",
+    link: "https://www.vargemgrandepaulista.sp.gov.br",
+    localidade: "Vargem Grande Paulista - SP", // Adicionei para facilitar a busca
 
-    {
-        // Informações sobre a cidade de Itapevi
-        titulo : "Itapevi",
-        descricao : "Coleta Seletiva: Todos os dias da semana nos períodos da manhã, tarde e noite",
-        link : "https://itapevi.sp.gov.br/coleta-de-lixo/",
-    },
+    // Array para os pontos de descarte e coleta específicos da cidade
+    pontosDeColeta: [
+      {
+        tipoDeResiduo: "Entulho e Móveis",
+        nomeServico: "SP Kata Tudo – Cata Bagulho",
+        contato: "WhatsApp: (11) 95132-9182",
+        observacoes: "Coleta de entulho, móveis e eletrodomésticos. Atendimento em domicílio.",
 
-    {
-        // Informações sobre a cidade de Osasco
-        titulo : "Osasco",
-        descricao : "Coleta Seletiva: Todos os dias da semana nos períodos da manhã, tarde e noite",
-        link : "https://osasco.sp.gov.br/digres/#1683116870581-06458f53-a9c8",
-    },
+      },
+      {
+        tipoDeResiduo: "Entulho e Móveis",
+        nomeServico: "Rafa Entulhos",
+        contato: "https://www.rafaentulhos.com.br", // URL completo
+        observacoes: "Coleta de entulho com caçambas. Licenciamento ambiental e destinação correta.",
+        
+      },
+      {
+        tipoDeResiduo: "Lixo Eletrônico",
+        nomeServico: "Cintitec Ambiental",
+        contato: "https://www.cintitec.com", // URL completo
+        observacoes: "Coleta e reciclagem de eletrônicos.",
+        
+      },
+      {
+        tipoDeResiduo: "Lixo Eletrônico",
+        nomeServico: "Ecoassist",
+        contato: "https://www.ecoassist.com.br", // URL completo
+        observacoes: "Coleta de eletrônicos para pessoas físicas e empresas.",
     
+      },
+      {
+        tipoDeResiduo: "Óleo Usado",
+        nomeServico: "Universo Ambiental",
+        contato: "https://universoambiental.eco.br", // URL completo
+        observacoes: "Coleta de óleos e lubrificantes usados.",
+      
+      },
+      {
+        tipoDeResiduo: "Óleo Usado",
+        nomeServico: "Preserva Recicla",
+        contato: "https://preservarecicla.com.br", // URL completo
+        observacoes: "Coleta e reciclagem de óleos e gorduras residuais.",
+      
+      },
+      {
+        tipoDeResiduo: "Recicláveis",
+        nomeServico: "Programa de Coleta Seletiva – Prefeitura",
+        contato: "Contato: (11) 4158-8800",
+        observacoes: "Coleta seletiva em fase de teste nos bairros Jardim Floresta, Marcopolo e condomínios.",
+        
+      },      
+      {
+        tipoDeResiduo: "Coleta Domiciliar",
+        nomeServico: "Prefeitura de Vargem Grande Paulista",
+        contato: "Contato: (11) 4158-8800",
+        observacoes: "Sistema de coleta de lixo domiciliar que abrange todo o município.",
+        
+      }
+            
+    ]
+  },
+  // outras cidades:
+  // {
+  //   titulo: "Outra Cidade",
+  //   descricao: "...",
+  //   link: "...",
+  //   pontosDeColeta: []
+  // }
 ];
-
